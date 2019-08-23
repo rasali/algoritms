926160e09dd292c4c7c16530c450e129bdd26424
RassulAli <rasul.aliyev94@gmail.com>
2019-08-23 12:06:13 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php 
+//Finding the max value from an array
+
+//$a = array(12,1,2,5,45,51,67,69,87,99,22,31,40,78,82,11);
+//$b = 0;
+
+//foreach ($a as $c) {
+//	if ($c > $b) {
+//		$b = $c;
+//	}
+//}
+
+//echo "The max value is " . $b;
+
+
+
+
+
+
+// Finding lowest value from an Array
+// $arr = [4,5,6,7,8,2,9,11];
+// $val = $arr[0];
+// $n = count($arr);
+
+// for($i=1;$i<$n;$i++) {
+// if($val<$arr[$i]) {
+//     $val = $val;        
+// } else {
+//     $val = $arr[$i];
+// }
+// }
+// print($val);
+
+
+
+
+
+
+
+
+//Sade edelerin tapilmasi
+
+
+// function sadeEded($eded){
+// 	for ($i=2; $i < $eded; $i++) { 
+// 		if ($eded % $i == 0) {
+// 			return $eded . ' sade eded deyil';
+// 		}
+// 	}
+// 	return $eded . ' sade ededdir';
+// }
+
+// echo sadeEded(5);
+
+
+ ?>
